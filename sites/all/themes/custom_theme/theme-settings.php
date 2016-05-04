aa1c5f751f2b412c7c70506beb204306c7dc2db0
mollieboots <mollie.bootsma@gmail.com>
2016-05-04 13:13:59 -0700

    change theme to non-broken one

--- sites/all/themes/custom_theme/theme-settings.php
+++ sites/all/themes/custom_theme/theme-settings.php
@@ -7,7 +7,7 @@
  * @param $form_state
  *   A keyed array containing the current state of the form.
  */
-function mollie_form_system_theme_settings_alter(&$form, &$form_state, $form_id = NULL)  {
+function custom_theme_form_system_theme_settings_alter(&$form, &$form_state, $form_id = NULL)  {
   // Work-around for a core bug affecting admin themes. See issue #943212.
   if (isset($form_id)) {
     return;
@@ -16,10 +16,10 @@ function mollie_form_system_theme_settings_alter(&$form, &$form_state, $form_id 
   // Create the form using Forms API: http://api.drupal.org/api/7
 
   /* -- Delete this line if you want to use this setting
-  $form['mollie_example'] = array(
+  $form['custom_theme_example'] = array(
     '#type'          => 'checkbox',
-    '#title'         => t('mollie sample setting'),
-    '#default_value' => theme_get_setting('mollie_example'),
+    '#title'         => t('custom_theme sample setting'),
+    '#default_value' => theme_get_setting('custom_theme_example'),
     '#description'   => t("This option doesn't do anything; it's just an example."),
   );
   // */

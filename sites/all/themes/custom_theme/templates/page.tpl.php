86a946439ce6220a8d0538abbcfba63d3999682e
mollieboots <mollie.bootsma@gmail.com>
2016-05-04 14:09:04 -0700

    basic bloacking out of theme and portfolio/home pages added

--- sites/all/themes/custom_theme/templates/page.tpl.php
+++ sites/all/themes/custom_theme/templates/page.tpl.php
@@ -30,22 +30,6 @@
       </div>
     <?php endif; ?>
 
-    <?php if ($secondary_menu): ?>
-      <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
-        <?php print theme('links__system_secondary_menu', array(
-          'links' => $secondary_menu,
-          'attributes' => array(
-            'class' => array('links', 'inline', 'clearfix'),
-          ),
-          'heading' => array(
-            'text' => $secondary_menu_heading,
-            'level' => 'h2',
-            'class' => array('element-invisible'),
-          ),
-        )); ?>
-      </nav>
-    <?php endif; ?>
-
     <?php print render($page['header']); ?>
 
   </header>
@@ -75,23 +59,41 @@
 
       <?php if ($main_menu): ?>
         <nav id="main-menu" role="navigation" tabindex="-1">
-          <?php
-          // This code snippet is hard to modify. We recommend turning off the
-          // "Main menu" on your sub-theme's settings form, deleting this PHP
-          // code block, and, instead, using the "Menu block" module.
-          // @see https://drupal.org/project/menu_block
-          print theme('links__system_main_menu', array(
-            'links' => $main_menu,
-            'attributes' => array(
-              'class' => array('links', 'inline', 'clearfix'),
-            ),
-            'heading' => array(
-              'text' => t('Main menu'),
-              'level' => 'h2',
-              'class' => array('element-invisible'),
-            ),
-          )); ?>
+            <div class="pull-left">
+                <?php
+                // This code snippet is hard to modify. We recommend turning off the
+                // "Main menu" on your sub-theme's settings form, deleting this PHP
+                // code block, and, instead, using the "Menu block" module.
+                // @see https://drupal.org/project/menu_block
+                print theme('links__system_main_menu', array(
+                    'links' => $main_menu,
+                    'attributes' => array(
+                        'class' => array('links', 'inline', 'clearfix'),
+                    ),
+                    'heading' => array(
+                        'text' => t('Main menu'),
+                        'level' => 'h2',
+                        'class' => array('element-invisible'),
+                    ),
+                )); ?>
+            </div>
+        <!-- </nav> -->
+        <!-- <nav class="header__secondary-menu" id="secondary-menu" role="navigation"> -->
+            <div class="pull-right">
+                <?php print theme('links__system_secondary_menu', array(
+                    'links' => $secondary_menu,
+                    'attributes' => array(
+                        'class' => array('links', 'inline', 'clearfix'),
+                    ),
+                    'heading' => array(
+                        'text' => $secondary_menu_heading,
+                        'level' => 'h2',
+                        'class' => array('element-invisible'),
+                    ),
+                )); ?>
+            </div>
         </nav>
+
       <?php endif; ?>
 
       <?php print render($page['navigation']); ?>
